4a5f1953abbd187fc843496a9a668359738e7318
Ellis Delgado <ellisdelgado13@gmail.com>
2021-06-29 14:11:43 -0700

    Creating home page

---
+++ client/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import homeGif from "../../src/components/home/homeGif";
+
+const Home = () => {
+
+    return (
+        <div className="mainContainer">
+            <homeGif />
+        </div>
+    )
+}
+export default Home;
--- client/src/components/home/homeGif.js
+++ client/src/components/home/homeGif.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const homeGif = () => {
+
+    return (
+        <div className="gifContainer">
+            <img src="bigMode.gif" id="bigMode" />
+        </div>
+    )
+}
+export default homeGif;
